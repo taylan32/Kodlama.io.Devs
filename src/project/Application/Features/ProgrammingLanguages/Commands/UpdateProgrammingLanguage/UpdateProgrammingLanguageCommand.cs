0af07cb99254a07ccdf4a039833132422b14a1a0
Taylan <taylanalparslan@hotmail.com>
2022-09-02 13:36:23 +0300

    CRUD operations added.

---
+++ src/project/Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommand.cs
@@ -0,0 +1,49 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommand : IRequest<UpdatedProgrammingLanguageDto>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class UpdateProgrammingLanguageCommandHandler : IRequestHandler<UpdateProgrammingLanguageCommand, UpdatedProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public UpdateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<UpdatedProgrammingLanguageDto> Handle(UpdateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                
+                await _programmingLanguageBusinessRules.ProgrammingLanguageShouldExistWhenUpdated(request.Id);
+                await _programmingLanguageBusinessRules.ProgrammingLanguageNameCannotBeDuplicatedWhenInserted(request.Name);
+
+                ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == request.Id);
+                _mapper.Map(request, programmingLanguage);
+                ProgrammingLanguage updatedProgrammingLanguage = await _programmingLanguageRepository.UpdateAsync(programmingLanguage);
+                UpdatedProgrammingLanguageDto updatedProgrammingLanguageDto = _mapper.Map<UpdatedProgrammingLanguageDto>(updatedProgrammingLanguage);
+
+                return updatedProgrammingLanguageDto;
+               
+            }
+        }
+    }
+}
---
+++ src/project/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,69 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCannotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming language name exists.");
+        }
+
+        public async Task ProgrammingLanguageShouldExistWhenRequested(int id)
+        {
+            ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == id);
+            if (programmingLanguage == null)
+            {
+                throw new BusinessException("Requested programming language does not exist");
+            }
+        }
+
+
+        public void ProgrammingLanguageShouldExistWhenRequested(ProgrammingLanguage programmingLanguage)
+        {
+            if (programmingLanguage == null)
+            {
+                throw new BusinessException("Requested programming language does not exist");
+            }
+
+        }
+
+        public async Task ProgrammingLanuageNameCannotBeDuplicatedWhenUpdated(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming language name exists.");
+        }
+
+        public async Task ProgrammingLanguageShouldExistWhenUpdated(int id)
+        {
+            ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == id);
+            if (programmingLanguage == null)
+            {
+                throw new BusinessException("Requested programming language does not exist");
+            }
+        }
+
+        public async Task ProgrammingLanguageShouldExistWhenDeleted(int id)
+        {
+            ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == id);
+            if (programmingLanguage == null) throw new BusinessException("Programming language does not exist.");
+        }
+
+    }
+}
